a25e1029a8f4604f294ef7a1f4871ca4fce7dfd6
Vimit <talk2vimit@gmail.com>
2024-02-25 22:39:31 +0530

    Initial commit

---
+++ aws_services/ec2_services/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Ec2ServicesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ec2_services"
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Start the project in detached mode
+docker-compose up -d
